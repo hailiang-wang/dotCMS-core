2a0ba85da72ac196760a7781bbd1732f3891c85c
dotcmsbuild <dotcmsbuild@dotcms.com>
2022-12-15 17:38:16 -0500

    Remove PowerMockRunner reference

--- dotCMS/src/test/java/com/dotcms/filters/interceptor/jwt/JsonWebTokenInterceptorTest.java
+++ dotCMS/src/test/java/com/dotcms/filters/interceptor/jwt/JsonWebTokenInterceptorTest.java
@@ -37,20 +37,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+
 
 /**
  * The goal of this unit test is to try some scenarios for the json web token interceptor.
  * @author jsanca
  */
-@PowerMockIgnore({"javax.management.*", "javax.crypto.*", "jdk.internal.reflect.*"})
-@PrepareForTest({ClusterFactory.class})
-@RunWith(PowerMockRunner.class)
 public class JsonWebTokenInterceptorTest extends UnitTestBase {
 
     final String clusterId = "CLUSTER-123";
